903e3b856eb2b594311b3e2eb5dc8370492bc99c
luckyyeah <yeahrh@163.com>
2016-10-11 18:20:11 +0800

    修改跳转

--- src/main/java/com/fh/controller/main/WapV3HomeController.java
+++ src/main/java/com/fh/controller/main/WapV3HomeController.java
@@ -115,7 +115,8 @@ public class WapV3HomeController extends BaseController {
 		
 		mapHomeData.put("columnDataList", columnDataList);
 		mapHomeData.put("columnId", columnId);
-		return  new ModelAndView("redirect:http://0913.enbozm.com:8090/emovie/wapv3/listRecommendVideo/" +CHANNEL_NO +"/"+columnId );
+		//return  new ModelAndView("redirect:http://1010h5.qzhe800.com/emovie/wapv3/listRecommendVideo/" +CHANNEL_NO +"/"+columnId );
+		return  new ModelAndView("redirect:/wapv3/listRecommendVideo/" +CHANNEL_NO +"/"+columnId );
 		//return  new ModelAndView("redirect:/wapv2/listColumnVideo/" +CHANNEL_NO +"/"+columnId );
 	}
 
